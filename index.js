7c7b11c8fb668b08afbe2ebc6e260d918400db43
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-01 22:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function iLoveTheBeatles(number){
   var showlove = []
   do {
     showlove.push ('I love the Beatles!');
-7 ++;}
-while (7 < 15);
+number ++;}
+while (number < 15);
 return showlove
 }
