aa057403ad5005a8abaa33bc50ef1cfb7302054e
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-01 21:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function johnLennonFacts(facts) {
   var excitingFacts = []
   var i = 0;
   while (i < facts.length) {
- facts[i] = facts[i] + "!!!";
+ excitingFacts[i] = facts[i] + "!!!";
   i++;
   }
 }
